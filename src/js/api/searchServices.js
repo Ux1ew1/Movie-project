18fc7094a5bd49618bfab75514c0922c37480abe
Ux1ew1 <ux1ew10220@gmail.com>
2024-06-09 14:13:59 +0300

    Добавит логику работы поиска фильма или сериала

--- src/js/api/searchServices.js
+++ src/js/api/searchServices.js
@@ -1,5 +1,7 @@
 import { getData } from "./getData.js";
 import { global } from "../global.js";
+import { cardsSearchTemplate } from "../utils/generateTemplate";
+import { smoothscroll } from "../components/smothScroll";
 
 /**
  * Выполняет запрос к API для поиска данных.
@@ -11,7 +13,6 @@ import { global } from "../global.js";
  */
 export const searchData = async () => {
   const endpoint = `search/${global.search.type}?query=${global.search.term}&page=${global.search.page}`;
-
   return getData(endpoint);
 };
 
@@ -20,21 +21,42 @@ export const searchData = async () => {
  * Извлекает параметры поиска из строки запроса URL, устанавливает глобальные параметры поиска и вызывает функцию поиска API.
  * Если результаты поиска не пустые, отображает их и очищает поле ввода.
  */
-export const search = async () => {};
+export const search = async () => {
+  // Передаём в term информацию о том что ищем из URL
+  global.search.term = new URL(window.location.href).searchParams.get(
+    "search-term"
+  );
+  // Передаём в type тип поиска
+  global.search.type = new URL(window.location.href).searchParams.get("type");
+
+  // Получаем данные из endpoint
+  const data = await searchData();
+  // Передаём данные о страницах в global
+  global.search.totalPages = data.total_pages;
+  // Передаём информацию о результатах поиска
+  global.search.totalResults = data.total_results;
+  // Деструктурируем данные из data
+  const { results } = data;
+  // Вызываем функцию для отрисовки карточек на странице поиска и передаём в неё данные из result
+  console.log(results);
+  displaySearchResults(results);
+};
 
 /**
  * Отображает результаты поиска.
  * @param {Array} results - Массив объектов с результатами поиска.
  */
 export const displaySearchResults = (results) => {
-  // Основная логика
-  // Отображаем пагинацию
+  // Вызываем функцию для отрисовки карточек и передаём в неё resault
+  cardsSearchTemplate(results, "#search-results");
+  displayPagination();
 };
 
 /**
  * Отображает пагинацию для результатов поиска.
  */
 export const displayPagination = () => {
+  document.querySelector("#pagination").innerHTML = "";
   const div = document.createElement("div");
 
   div.classList.add("pagination");
@@ -65,7 +87,8 @@ export const displayPagination = () => {
 
     // Получаем результаты поиска для новой страницы
     const { results } = await searchData();
-
+    displaySearchResults(results);
+    smoothscroll();
     // Отображаем результаты поиска для новой страницы
     // Плавная прокрутка к началу страницы
   });
@@ -76,6 +99,8 @@ export const displayPagination = () => {
 
     // Получаем результаты поиска для новой страницы
     const { results } = await searchData();
+    displaySearchResults(results);
+    smoothscroll();
 
     // Отображаем результаты поиска для новой страницы
     // Плавная прокрутка к началу страницы
