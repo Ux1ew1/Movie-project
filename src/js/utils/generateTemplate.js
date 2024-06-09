f725e14a333865840c06bfff2390829e07540901
Ux1ew1 <ux1ew10220@gmail.com>
2024-06-09 14:12:33 +0300

    Добавит шаблон для генерации карточек при поиске. Добавит функцию для получения id карточки и обработку клика по карточкам

--- src/js/utils/generateTemplate.js
+++ src/js/utils/generateTemplate.js
@@ -1,4 +1,6 @@
 import { initSwiper } from "../components/swiper.js";
+import { getData } from "../api/getData";
+import { showModalInfo } from "../components/modal";
 
 /**
  * Отображает список контента (фильмы или сериалы) в зависимости от указанного endpoint.
@@ -9,15 +11,14 @@ import { initSwiper } from "../components/swiper.js";
  * @returns {Promise} Объект Promise.
  */
 export const cardsTemplate = async (endpoint, containerSelector, idParam) => {
+  const { results } = await getData(endpoint);
   results.forEach((content) => {
     const div = document.createElement("div");
 
     div.classList.add("card");
 
     div.innerHTML = `
-      <a class="list-item-link" href="${idParam}-details.html?id=${
-      content?.id
-    }">
+      <div class="list-item-link" id="list-item-link" data-id="${content.id}">
         ${
           content.poster_path
             ? `<img src="https://www.themoviedb.org/t/p/w300${
@@ -39,11 +40,51 @@ export const cardsTemplate = async (endpoint, containerSelector, idParam) => {
                 : ""
             }
           </div>
-        </a>
+        </div>
       `;
 
     document.querySelector(containerSelector).appendChild(div);
   });
+  attachListeners();
+};
+
+// Функция для отрисовки карточек на странице поиска. 
+export const cardsSearchTemplate = async (data, containerSelector) => {
+  const results = data;
+  const container = document.querySelector(containerSelector);
+  container.innerHTML = "";
+  results.forEach((content) => {
+    const div = document.createElement("div");
+
+    div.classList.add("card");
+    div.innerHTML = `
+      <div class="list-item-link" id="list-item-link" data-id="${content.id}">
+      ${
+        content.poster_path
+          ? `<img src="https://www.themoviedb.org/t/p/w300${
+              content.poster_path
+            }" alt="${content?.title || content?.name}" />`
+          : `<div class="no-img"></div>`
+      }
+        <h3 class="list-item-rating">
+          <svg class="rating" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.15336 2.33977L10.3267 4.68643C10.4867 5.0131 10.9134 5.32643 11.2734 5.38643L13.4 5.73977C14.76 5.96643 15.08 6.9531 14.1 7.92643L12.4467 9.57977C12.1667 9.85977 12.0134 10.3998 12.1 10.7864L12.5734 12.8331C12.9467 14.4531 12.0867 15.0798 10.6534 14.2331L8.66003 13.0531C8.30003 12.8398 7.7067 12.8398 7.34003 13.0531L5.3467 14.2331C3.92003 15.0798 3.05336 14.4464 3.4267 12.8331L3.90003 10.7864C3.9867 10.3998 3.83336 9.85977 3.55336 9.57977L1.90003 7.92643C0.926698 6.9531 1.24003 5.96643 2.60003 5.73977L4.7267 5.38643C5.08003 5.32643 5.5067 5.0131 5.6667 4.68643L6.84003 2.33977C7.48003 1.06643 8.52003 1.06643 9.15336 2.33977Z" stroke="#FFAD49" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+            ${content.vote_average.toFixed(1)} / 10
+          </h3>
+        <div class="list-item-descr">
+          <h3 class="list-item-title">${content?.title || content?.name}</h3>
+          ${
+            content?.release_date
+              ? `<p class="list-item-subtitle">Release: <small class="list-item-muted">${content?.release_date}</small></p>`
+              : ""
+          }
+        </div>
+      </div>
+    `;
+    container.appendChild(div);
+  });
+  attachListeners();
 };
 
 /**
@@ -52,15 +93,14 @@ export const cardsTemplate = async (endpoint, containerSelector, idParam) => {
  * @param {string} endpoint - Конечная точка API, к которой нужно выполнить запрос.
  */
 export const cardsTemplateWithSlider = async (contentType, endpoint) => {
+  const { results } = await getData(endpoint);
   results.forEach((content) => {
     const div = document.createElement("div");
 
     div.classList.add("swiper-slide");
 
     div.innerHTML = `
-      <a class="slider-link" href="${contentType}-details.html?id=${
-      content.id
-    }">
+      <div class="slider-link" id="list-item-link" data-id=${content.id}>
         <img src="https://image.tmdb.org/t/p/w500${content.poster_path}" alt="${
       content.title
     }" />
@@ -78,11 +118,26 @@ export const cardsTemplateWithSlider = async (contentType, endpoint) => {
             }</small>
           </p>
         </div>
-      </a>
+      </div>
     `;
 
     document.querySelector(".swiper-wrapper").appendChild(div);
   });
 
   initSwiper();
+};
+
+// Функция для получения id и класса из элемента по которому кликнули 
+const click = async (event) => {
+  const id = event?.currentTarget?.dataset?.id ?? "N/A";
+  const cardClassName = event?.currentTarget?.className ?? "N/A";
+  showModalInfo(id, cardClassName); // Вызов модального окна 
+};
+
+// Фунция для обработки клика по карточкам. 
+const attachListeners = () => {
+  const cards = document.querySelectorAll("[data-id]"); // Получения всех карточек с data атрибутом id 
+  cards.forEach((card) => { 
+    card.addEventListener("click", click); // Вешаем обработчик клика на все карточки 
+  });
 };
