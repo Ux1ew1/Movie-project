97e6dd24ad3aedac6fe35891c803aef7d32bce61
Ux1ew1 <ux1ew10220@gmail.com>
2024-06-09 14:14:25 +0300

    Добавит вызов функции поиска

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import { global } from "./global.js";
+import {
+  cardsTemplate,
+  cardsTemplateWithSlider,
+} from "./utils/generateTemplate";
+import { tabsComponent } from "./components/tabs";
+import { search } from "./api/searchServices";
 
 /**
  * Инициализирует функции в зависимости от страницы.
@@ -9,15 +15,20 @@ function init() {
     case "/":
     case "/index.html":
       // Вызываем функции для отображения фильмов в прокате (слайдер), а также популярных фильмов и сериалов
+      cardsTemplateWithSlider("movie", "movie/now_playing");
 
+      cardsTemplate("movie/popular", ".popular-movies", "movie");
+      cardsTemplate("tv/popular", ".popular-tv", "tv");
+
+      tabsComponent();
       break;
     case "/movie-details.html":
-      // Вызываем функцию для отображения деталей о фильме
       break;
     case "/tv-details.html":
       // Вызываем функцию для отображения деталей о сериале
       break;
     case "/search.html":
+      search();
       // Вызываем функцию для выполнения поиска
       break;
   }
